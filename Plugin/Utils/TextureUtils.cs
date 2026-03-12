e0dc90152c44eebc279e1860b69dedae3f2ba6d9
acidphantasm <acidphantasm@acidphantasm.com>
2026-03-12 12:59:44 -0500

    Update map licenses, and add compression to maps on load

--- Plugin/Utils/TextureUtils.cs
+++ Plugin/Utils/TextureUtils.cs
@@ -6,27 +6,45 @@ namespace DynamicMaps.Utils
 {
     public static class TextureUtils
     {
-        private static Dictionary<string, Sprite> _spriteCache = new Dictionary<string, Sprite>();
+        private static Dictionary<string, Sprite> _spriteCache = new();
+        private static Dictionary<string, Texture2D> _mapCache = new();
 
         public static Texture2D LoadTexture2DFromPath(string absolutePath)
         {
-            if (!File.Exists(absolutePath))
-            {
+            if (string.IsNullOrEmpty(absolutePath) || !File.Exists(absolutePath))
                 return null;
-            }
+
+            if (_mapCache.TryGetValue(absolutePath, out var cachedTex))
+                return cachedTex;
 
             var tex = new Texture2D(2, 2, TextureFormat.RGBA32, false);
             tex.LoadImage(File.ReadAllBytes(absolutePath));
 
+            var newWidth = (tex.width + 3) & ~3;
+            var newHeight = (tex.height + 3) & ~3;
+
+            if (newWidth != tex.width || newHeight != tex.height)
+            {
+                var padded = new Texture2D(newWidth, newHeight, TextureFormat.RGBA32, false);
+
+                padded.SetPixels(0, 0, tex.width, tex.height, tex.GetPixels());
+                padded.Apply();
+
+                Object.Destroy(tex);
+                tex = padded;
+            }
+
+            tex.Compress(true);
+            tex.Apply(false, true);
+
+            _mapCache[absolutePath] = tex;
             return tex;
         }
 
         public static Sprite GetOrLoadCachedSprite(string path)
         {
-            if (_spriteCache.ContainsKey(path))
-            {
-                return _spriteCache[path];
-            }
+            if (_spriteCache.TryGetValue(path, out var sprite))
+                return sprite;
 
             var absolutePath = Path.Combine(Plugin.Path, path);
             var texture = LoadTexture2DFromPath(absolutePath);

787996bebc2f5e3f4808f5063991ebff7ae506fd
acidphantasm <acidphantasm@acidphantasm.com>
2026-03-14 02:29:42 -0500

    Go back to full RGBA32 Textures... for icons cause idgaf

--- Plugin/Utils/TextureUtils.cs
+++ Plugin/Utils/TextureUtils.cs
@@ -7,37 +7,14 @@ namespace DynamicMaps.Utils
     public static class TextureUtils
     {
         private static Dictionary<string, Sprite> _spriteCache = new();
-        private static Dictionary<string, Texture2D> _mapCache = new();
 
-        public static Texture2D LoadTexture2DFromPath(string absolutePath)
+        private static Texture2D LoadTexture2DFromPath(string absolutePath)
         {
             if (string.IsNullOrEmpty(absolutePath) || !File.Exists(absolutePath))
                 return null;
 
-            if (_mapCache.TryGetValue(absolutePath, out var cachedTex))
-                return cachedTex;
-
             var tex = new Texture2D(2, 2, TextureFormat.RGBA32, false);
             tex.LoadImage(File.ReadAllBytes(absolutePath));
-
-            var newWidth = (tex.width + 3) & ~3;
-            var newHeight = (tex.height + 3) & ~3;
-
-            if (newWidth != tex.width || newHeight != tex.height)
-            {
-                var padded = new Texture2D(newWidth, newHeight, TextureFormat.RGBA32, false);
-
-                padded.SetPixels(0, 0, tex.width, tex.height, tex.GetPixels());
-                padded.Apply();
-
-                Object.Destroy(tex);
-                tex = padded;
-            }
-
-            tex.Compress(true);
-            tex.Apply(false, true);
-
-            _mapCache[absolutePath] = tex;
             return tex;
         }
 
